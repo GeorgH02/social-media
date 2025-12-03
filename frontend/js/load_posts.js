cb45507eeee0add7200911ae4a5d1bc6be849602
st12udent3 <ai25m050@technikum-wien.at>
2025-12-03 10:10:54 +0100

    Added frontend filters for posts site

--- frontend/js/load_posts.js
+++ frontend/js/load_posts.js
@@ -1,4 +1,107 @@
 $(document).ready(function() {
+
+    const categories = ["Europe", "Asia", "Africa", "North America", "South America", "Australia", "Antarctica"];
+    const subcategories = {
+        "Europe": ["Austria", "Italy", "United Kingdom"],
+        "Asia": ["China", "India"],
+        "Africa": ["South Africa"],
+        "North America": ["United States", "Canada", "Mexico"],
+        "South America": ["Brazil"],
+        "Australia": ["Australia"],
+        "Antarctica": []
+    };
+    const filters = ["City", "Nature"];
+
+    let selectedCategories = [];
+    let selectedFilters = [];
+
+    function showCategories() {
+    let categoryItems = categories.map(category => `
+        <li class="nav-item">
+            <a class="nav-link category" href="#" data-category="${category}">${category}</a>
+        </li>
+    `).join("");
+    $("#categoryItems").html(categoryItems);
+    $("#categoryNavbar").removeClass("d-none");
+    }
+
+    function showSubcategories(category) {
+        let subcategoryItems = subcategories[category].map(subcategory => `
+            <li class="nav-item">
+                <a class="nav-link subcategory" href="#" data-subcategory="${subcategory}">${subcategory}</a>
+            </li>
+        `).join("");
+        $("#categoryItems").html(subcategoryItems);
+    }
+
+    function showFilters() {
+        if ($("#filterItems").children().length === 0) {
+        let filterItems = filters.map(filter => `
+            <li class="nav-item">
+                <a class="nav-link filter" href="#" data-filter="${filter}">${filter}</a>
+            </li>
+        `).join("");
+        $("#filterItems").html(filterItems);
+        }
+        $("#filterNavbar").removeClass("d-none");
+    }
+
+    showCategories();
+
+    $(document).on("click", ".category", function (event) {
+        event.preventDefault();
+        const category = $(this).data("category");
+
+        if ($(this).hasClass("bg-primary text-white")) {
+        $(this).removeClass("bg-primary text-white");
+        $("#backToCategories").addClass("d-none");
+        return;
+        }
+
+        $(".category").removeClass("bg-primary text-white");
+        $(this).addClass("bg-primary text-white");
+
+        showSubcategories(category);
+
+        $("#backToCategories").removeClass("d-none");
+    });
+
+    $(document).on("click", ".subcategory", function (event) {
+        event.preventDefault();
+        const subcategory = $(this).data("subcategory");
+        $(this).toggleClass("bg-primary text-white");
+        if ($(this).hasClass("bg-primary")) {
+        selectedCategories.push(subcategory);
+        } else {
+        selectedCategories = selectedCategories.filter(item => item !== subcategory);
+        }
+    });
+
+    $(document).on("click", "#backToCategories", function () {
+        showCategories();
+        $("#backToCategories").addClass("d-none");
+    });
+
+    $(document).on("click", ".filter", function (event) {
+        event.preventDefault();
+        const filter = $(this).data("filter");
+        $(this).toggleClass("bg-secondary text-white");
+        if ($(this).hasClass("bg-secondary")) {
+        selectedFilters.push(filter);
+        } else {
+        selectedFilters = selectedFilters.filter(item => item !== filter);
+        }
+    });
+
+    $(document).on("click", "#showFilters", function (event) {
+        event.preventDefault();
+        if ($("#filterNavbar").hasClass("d-none")) {
+        showFilters();
+        } else {
+        $("#filterNavbar").addClass("d-none");
+        }
+    });
+
     function fetchPosts() {
         const path = window.location.pathname; // current path
 
--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -3,44 +3,48 @@ $(document).ready(function () {
 
   function updateNavbar() {
     const loggedInUser = localStorage.getItem("loggedInUser");
-    let navbarContent = `
+    let leftContent = `
             <li class="nav-item">
                 <a class="nav-link" href="/posts">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/create_post">Create a new Post</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/users">Users</a>
             </li>
         `;
 
-    $("#navbarNav .me-auto").html(navbarContent);
+    
 
     let rightContent = "";
     if (loggedInUser) {
         rightContent += `
-                <li class="nav-item dropdown profile">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Hello, ${loggedInUser}
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="">My Profile</a></li>
-                        <li><a class="dropdown-item" href="">My Content</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="" id="sign-out">Logout</a></li>
-                    </ul>
-                </li>
-            `;
+            <li class="nav-item dropdown profile">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Hello, ${loggedInUser}
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="">My Profile</a></li>
+                    <li><a class="dropdown-item" href="">My Content</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="" id="sign-out">Logout</a></li>
+                </ul>
+            </li>
+        `;
+
+        leftContent += `
+            <li class="nav-item">
+                <a class="nav-link" href="/create_post">Create a new Post</a>
+            </li>
+        `;
+
     } else {
-               rightContent += `
-                            <li class="nav-item">
-                            <a class="nav-link" href="/login">Login</a>
-                            </li>
-            `; 
+        rightContent += `
+            <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+            </li>
+        `; 
     }
     
-    
+    $("#navbarNav .me-auto").html(leftContent);
     $("#right-nav-items").html(rightContent);
   }
 
